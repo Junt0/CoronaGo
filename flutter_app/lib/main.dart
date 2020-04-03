6c2c54dad66cf973c6cbbb80c2cd634fd186b235
kalinkochnev <kalin.kochnev@gmail.com>
2020-04-03 13:49:18 -0400

    main.dart: Testing of bar code generation and scanning works

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
 
 void main() {
   runApp(new MyApp());
@@ -14,51 +17,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String barcode = "";
+  Widget random_code = Text('No qr code has been generated');
+	String code_str = "";
+	String scanned_code = "";
 
   @override
   initState() {
     super.initState();
+		randomQr();
   }
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Barcode Scanner Example'),
-          ),
-          body: new Center(
-            child: new Column(
+        home: new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Barcode Scanner Testing'),
+              backgroundColor: Colors.deepPurple,
+            ),
+            body: SafeArea(
+							child: Center(
+							  child: Column(
               children: <Widget>[
-                new Container(
-                  child: new MaterialButton(
-                      onPressed: scan, child: new Text("Scan")),
-                  padding: const EdgeInsets.all(8.0),
-                ),
-                new Text(barcode),
+							  	Text('Scanned code: $scanned_code'),
+                Container(
+							  		child: RaisedButton(
+							  			onPressed: scan,
+							  			child: Text('Scan a code'),
+							  			padding: EdgeInsets.all(8.0),
+                	)),
+							  	Container(
+							  		child: RaisedButton(
+							  			onPressed: randomQr,
+							  			child: Text('Generate a brand new code')
+							  		)
+							  	),
+							  	Text('Set code: $code_str'),
+							  	this.random_code,
               ],
             ),
-          )),
-    );
+							))));
   }
 
   Future scan() async {
     try {
       String barcode = await BarcodeScanner.scan();
-      setState(() => this.barcode = barcode);
+      setState(() => this.scanned_code = barcode);
     } on PlatformException catch (e) {
       if (e.code == BarcodeScanner.CameraAccessDenied) {
         setState(() {
-          this.barcode = 'The user did not grant the camera permission!';
+          this.scanned_code = 'The user did not grant the camera permission!';
         });
       } else {
-        setState(() => this.barcode = 'Unknown error: $e');
+        setState(() => this.scanned_code = 'Unknown error: $e');
       }
-    } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
+    } on FormatException {
+      setState(() => this.scanned_code =
+          'null (User returned using the "back"-button before scanning anything. Result)');
     } catch (e) {
-      setState(() => this.barcode = 'Unknown error: $e');
+      setState(() => this.scanned_code = 'Unknown error: $e');
     }
   }
+
+	void randomQr() {
+		Random rand = new Random();
+
+		setState(() {
+			this.code_str = rand.nextInt(9999999).toString();
+			this.random_code = new QrImage(
+				data: this.code_str,
+				version: QrVersions.auto,
+				size: 200.0,
+			);
+		});
+		print("Generated code ${this.code_str}");
+
+	} 
+
 }
