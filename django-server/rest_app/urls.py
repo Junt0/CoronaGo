c7e655fedbb73a2b770ad6e9f4afb021b25c15ce
kalinkochnev <kalin.kochnev@gmail.com>
2020-04-04 21:51:36 -0400

    urls.py: Modified urls to be more consistent in address

--- django-server/rest_app/urls.py
+++ django-server/rest_app/urls.py
@@ -22,13 +22,13 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', views.home, name="home"),
 
-    path('api/interaction/create', views.CreateInteraction.as_view(), name="create_interaction"),
-    path('api/interaction/join/<uuid:code>', views.JoinInteraction.as_view(), name="join_interaction"),
-    path('api/interaction/end/<uuid:code>', views.EndInteraction.as_view(), name='end_interaction'),
-    path('api/user/<int:pk>', views.RequestUserProf.as_view(), name="profile_info"),
+    path('api/interaction/create/', views.CreateInteraction.as_view(), name="create_interaction"),
+    path('api/interaction/join/<uuid:code>/', views.JoinInteraction.as_view(), name="join_interaction"),
+    path('api/interaction/end/<uuid:code>/', views.EndInteraction.as_view(), name='end_interaction'),
+    path('api/user/<int:pk>/', views.RequestUserProf.as_view(), name="profile_info"),
 
     path('api/auth/', views.AuthGetToken.as_view(), name="get_token"),
-    path('api/auth/signup', views.AuthSignup.as_view(), name="signup"),
+    path('api/auth/signup/', views.AuthSignup.as_view(), name="signup"),
     re_path(r'^api/auth/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             views.VerifyAccount.as_view(), name='verify_email'),
 
