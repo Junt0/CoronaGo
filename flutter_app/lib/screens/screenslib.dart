76727932421861a45fb73df8af842bf849b51b5f
kalinkochnev <kalin.kochnev@gmail.com>
2020-04-08 19:31:27 -0400

    commiting outstanding changes

--- flutter_app/lib/screens/screenslib.dart
+++ flutter_app/lib/screens/screenslib.dart
@@ -1,4 +1,6 @@
 library screens;
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/services/api_helper.dart';
@@ -6,7 +8,7 @@ import 'package:flutter_app/services/user_service.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hive/hive.dart';
 
-part "app_open.dart";
+part "splash_screen.dart";
 part "auth.dart";
 part "home.dart";
 part "login.dart";
