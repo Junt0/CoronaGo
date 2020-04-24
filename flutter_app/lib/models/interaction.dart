82a9feeb4e25fb5c5dd94a48ae440c3057569f19
kalinkochnev <kalin.kochnev@gmail.com>
2020-04-24 18:34:19 -0400

    interaction.dart: Interaction extends cached class

--- flutter_app/lib/models/interaction.dart
+++ flutter_app/lib/models/interaction.dart
@@ -3,22 +3,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/models/profile.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/services/api_helper.dart';
+import 'package:flutter_app/services/caching.dart';
 
 import 'auth_user.dart';
 
-class Interaction {
+class Interaction extends CachedClass {
+  String key;
+
   String _uuid = "";
   List<Profile> _participants;
   DateTime _meetTime;
   DateTime _endTime;
 
-  Interaction();
+  DateTime lastUpdated;
+  Map<String, Function> validators;
+
+  Interaction(){
+    this._loadValidators();
+  }
+
+  Map<String, dynamic> toCacheFormat({bool excludeKey = true}) {  
+    List excludedAttributes = (excludeKey) ? ['key'] : [];
+    return attributesToMap(excludedAttributes, exclude: true);
+  }
+
+  Interaction fromCacheFormat(Map<String, dynamic> fromCache) {
+    Interaction inter = new Interaction();
+
+    inter.uuid = fromCache['uuid'];
+    inter.key = _uuid;
+    inter.participants = this._loadParticipantsCache(fromCache['participants']);
+    inter.meetTime = this._loadDateTime(fromCache['meetTime']);
+    inter.endTime = this._loadDateTime(fromCache['endTime']);
+    inter.lastUpdated = this._loadDateTime(fromCache['lastUpdated']);
+
+    return inter;
+  }
+
+  void _loadValidators() {
+    Map<String, Function> validators = {
+      'uuid': CachedClass.isUUID,
+      'participants': Profile.isProfileList,
+    };
+    this.validators = validators;
+  }
 
   Interaction.fromResponse(Map<String, dynamic> parsedJson) {
+    this._loadValidators();
     _uuid = parsedJson['interaction_code'];
-    _participants = this._loadParticipants(parsedJson['participants']);
+    _participants = this._loadParticipantsJson(parsedJson['participants']);
     _meetTime = this._loadDateTime(parsedJson['meet_time']);
     _endTime = this._loadDateTime(parsedJson['end_time']);
+    lastUpdated = this._loadDateTime(parsedJson['last_updated']);
   }
 
   static Future<Interaction> fromQrCode(AuthUser user, GlobalKey<ScaffoldState> scaffoldKey) async {
@@ -38,10 +74,53 @@ class Interaction {
     }
   }
 
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> attributes = {
+      'uuid': this._uuid,
+      'key': this.key,
+      'lastUpdated': this.lastUpdated.toIso8601String(),
+      'participants': this._participantsToMaps(),
+      'meetTime': this._meetTime.toIso8601String(),
+      'endTime': this._endTime.toIso8601String(),
+    };
+
+    return attributes;
+  }
+
+  List<Map<String, dynamic>> _participantsToMaps() {
+    List<Map<String, dynamic>> participantMaps = [];
+    for (Profile prof in this._participants) {
+      participantMaps.add(prof.toMap());
+    }
+    return participantMaps;
+  }
+
+  Map<String, dynamic> attributesToMap(List<String> attributeNames, {bool exclude=false}) {
+    Map allFields = this.toMap();
+
+    if (exclude) {
+      for (String name in attributeNames) {
+        allFields.remove(allFields[name]);
+      } 
+      return allFields;
+
+    } else {
+      Map<String, String> parsedFieldMap = new Map<String, String>();
+      for (String name in attributeNames) {
+        parsedFieldMap[name] = allFields[name];
+      } 
+      return parsedFieldMap;
+    }
+  }
+
   String getUUID() => _uuid;
   List<Profile> getParticipants() => _participants;
   DateTime getMeetTime() => _meetTime;
   DateTime getEndTime() => _endTime;
+  set uuid(String uuid) => this._uuid = uuid;
+  set participants(List<Profile> profiles) => this._participants = profiles;
+  set meetTime(DateTime time) => this._meetTime = time;
+  set endTime(DateTime time) => this._endTime = time;
 
   void setUUID(String uuid) {
     this._uuid = uuid;
@@ -53,10 +132,18 @@ class Interaction {
     return regExp.hasMatch(uuid);
   }
 
-  List<Profile> _loadParticipants(List<Map> particpants) {
+  List<Profile> _loadParticipantsJson(List<Map> particpants) {
     List<Profile> profiles = new List<Profile>();
     for (Map<String, dynamic> profileJson in particpants) {
       profiles.add(Profile.fromJson(profileJson));
+    }
+    return profiles;
+  }
+
+  List<Profile> _loadParticipantsCache(List<Map> particpants) {
+    List<Profile> profiles = new List<Profile>();
+    for (Map<String, dynamic> profile in particpants) {
+      profiles.add(new Profile()..fromCacheFormat(profile));
     }
     return profiles;
   }
